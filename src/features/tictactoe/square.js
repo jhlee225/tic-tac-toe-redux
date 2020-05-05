db1ab22990d9d8696741d260d8f1b51cd6bbae7f
Kasi <57307447+jhlee225@users.noreply.github.com>
2020-05-05 11:58:49 +0900

    Move winner state to Redux

--- src/features/tictactoe/square.js
+++ src/features/tictactoe/square.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { connect } from "react-redux";
-import { handleClick } from "./tttSlice";
+import { calculateWinner, handleClick } from "./tttSlice";
 
 function Square(props) {
-  const { id, value, history, stepNumber, xIsNext, handleClick } = props;
+  const { winner, history, stepNumber, xIsNext } = props;
+  const { id, value, calculateWinner, handleClick } = props;
   function squareClick(i) {
     const temp = history.slice(0, stepNumber + 1);
     const current = temp[temp.length - 1];
     const squares = current.squares.slice();
-    const winner = calculateWinner(squares);
-
+    calculateWinner({ squares });
+    console.log(winner);
     if (winner || squares[i.target.id]) {
       return;
     }
@@ -24,6 +25,7 @@ function Square(props) {
       xIsNext: !xIsNext,
     };
     handleClick({ state: state });
+    calculateWinner({ squares });
   }
   return (
     <button className="square" id={id} onClick={(e) => squareClick(e)}>
@@ -32,37 +34,19 @@ function Square(props) {
   );
 }
 
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-
 function mapStateToProps(state) {
   return {
     history: state.ttt.history,
     stepNumber: state.ttt.stepNumber,
     xIsNext: state.ttt.xIsNext,
+    winner: state.ttt.winner,
   };
 }
 
 function mapDispatchToProps(dispatch) {
   return {
     handleClick: ({ state }) => dispatch(handleClick({ state })),
+    calculateWinner: ({ squares }) => dispatch(calculateWinner({ squares })),
   };
 }
 
